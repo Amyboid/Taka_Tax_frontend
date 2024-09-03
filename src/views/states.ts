dced56e6d3ca1db5559b3f508496f1a16753acd0
ss <134444582+Amyboid@users.noreply.github.com>
2024-09-03 22:14:12 +0530

    final commit

--- src/views/states.ts
+++ src/views/states.ts
@@ -2,4 +2,4 @@ import { atom } from "jotai";
 
 export const amountAtom = atom(0); 
 export const purposeAtom = atom("")
-export const splitsAtom = atom<string[]>([]); 
+export const splitsAtom = atom<string[]>([]);
